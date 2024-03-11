1c34bd57f95cbace8935e28c88f581cec2a629d1
Kenneth Wilson <kennethwilsonherrera@gmail.com>
2024-03-10 21:44:37 -0700

    March 10 2024

--- src/test/java/group_01/BaseTest.java
+++ src/test/java/group_01/BaseTest.java
@@ -5,8 +5,13 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.time.Duration;
 
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.RemoteWebElement;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+
+import com.google.common.collect.ImmutableMap;
 
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.options.UiAutomator2Options;
@@ -32,6 +37,34 @@ public class BaseTest {
 		
 		driver = new AndroidDriver(new URL("http://127.0.0.1:4723"), options);
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+	}
+	
+	public void longPressAction(WebElement ele) {
+		((JavascriptExecutor)driver).executeScript("mobile: longClickGesture", 
+				ImmutableMap.of(
+						"elementId", ((RemoteWebElement) ele).getId(),
+						"duration", 2000
+						)
+				);
+	}
+	
+	public void scrollToEndAction() {
+		boolean canScrollMore;
+		do {
+			canScrollMore = (Boolean) ((JavascriptExecutor) driver).executeScript("mobile: scrollGesture", ImmutableMap.of(
+			    "left", 100, "top", 100, "width", 200, "height", 200,
+			    "direction", "down",
+			    "percent", 3.0
+			));
+		}while(canScrollMore);
+	}
+	
+	public void swipeAction(WebElement ele, String direction) {
+		((JavascriptExecutor) driver).executeScript("mobile: swipeGesture", ImmutableMap.of(
+			    "elementId", ((RemoteWebElement)ele).getId(),
+			    "direction", direction,
+			    "percent", 0.75
+			));
 	}
 	
 	@AfterClass
