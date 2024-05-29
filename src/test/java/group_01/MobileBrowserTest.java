49c9d68d2e21cf364bc676287608a562bcaad01b
Kenneth Wilson <kennethwilsonherrera@gmail.com>
2024-05-28 22:39:58 -0700

    May 28 2024

--- src/test/java/group_01/MobileBrowserTest.java
+++ src/test/java/group_01/MobileBrowserTest.java
@@ -1,17 +1,30 @@
 package group_01;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Keys;
+import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import com.google.common.collect.ImmutableMap;
 
 public class MobileBrowserTest extends BrowserBaseTest {
 
 	@Test
-	public void browserTest() {
-		driver.get("http://google.com");
-		System.out.println(driver.getTitle());
-		driver.findElement(By.name("q")).sendKeys("rahul shetty academy");
-		driver.findElement(By.name("q")).sendKeys(Keys.ENTER);
+	public void browserTest() throws InterruptedException {
+//		driver.get("http://google.com");
+//		System.out.println(driver.getTitle());
+//		driver.findElement(By.name("q")).sendKeys("rahul shetty academy");
+//		driver.findElement(By.name("q")).sendKeys(Keys.ENTER);
 		//Thread.sleep(3000);
+		driver.get("https://rahulshettyacademy.com/angularAppdemo/");
+		driver.findElement(By.xpath("//span[@class='navbar-toggler-icon']")).click();
+		
+		driver.findElement(By.cssSelector("a[routerlink*='products']")).click();
+		((JavascriptExecutor) driver).executeScript("window.scrollBy( 0,1000)", "");
+		
+		String text = driver.findElement(By.cssSelector("a[href*='products/3']")).getText();
+		Assert.assertEquals(text, "Devops");
+		Thread.sleep(3000);
 	}
 }
